ce27c20da38cf6334a3bfefc0931a63c9cd83202
Mariewbm <marie.thomassin@laplateforme.iogit config --global user.email marie.thomassin@laplateforme.io>
2025-03-20 09:33:45 +0100

    finalisation des tables et insertions de valeurs

--- badbunny.sql
+++ badbunny.sql
@@ -8,6 +8,12 @@ surname VARCHAR(255) NOT NULL,
 email VARCHAR(266) NOT NULL UNIQUE,
 password_hash VARCHAR(255) NOT NULL);
 
+INSERT INTO users (name, surname, email, password_hash) 
+VALUES 
+('Alice', 'Dupont', 'alice.dupont@email.com', 'motdepasse1'),
+('John', 'Doe', 'john.doe@email.com', 'motdepasse2'),
+('Emma', 'Martin', 'emma.martin@email.com', 'motdepasse3');
+
 CREATE TABLE accounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     account_name VARCHAR(255) NOT NULL,
@@ -15,6 +21,13 @@ CREATE TABLE accounts (
     id_users INT,
     FOREIGN KEY (id_users) REFERENCES users(id) ON DELETE CASCADE
 );
+
+INSERT INTO accounts (account_name, balance, id_users) 
+VALUES 
+('Dupont', 1500.00, 1),
+('Doe', 800.00, 2),
+('Martin', 3000.00, 3);
+
 
 CREATE TABLE transactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -26,6 +39,12 @@ CREATE TABLE transactions (
     id_accounts INT,
     FOREIGN KEY (id_category) REFERENCES category(id) ON DELETE SET NULL,
     FOREIGN KEY (id_accounts) REFERENCES accounts(id) ON DELETE CASCADE);
+    
+INSERT INTO transactions (description, amount, transaction_date, type, id_category, id_accounts) 
+VALUES 
+('Achat supermarché', 45.30, '2025-03-18 14:30:00', 'deposit', 1, 1),
+('Virement salaire', 2000.00, '2025-03-17 16:45:00', 'transfer', 2, 2),
+('Facture internet', 35.99, '2025-03-16 08:30:00', 'withdrawal', 3, 3);
 
 CREATE TABLE transfers (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -36,6 +55,11 @@ CREATE TABLE transfers (
     FOREIGN KEY (from_account) REFERENCES accounts(id) ON DELETE CASCADE,
     FOREIGN KEY (to_account) REFERENCES accounts(id) ON DELETE CASCADE
 );
+INSERT INTO transfers (from_account, to_account, amount, transfert_date) 
+VALUES 
+(1, 2, 100.00, '2025-02-19 19:20:00'),
+(2, 3, 250.00, '2025-01-10 12:56;00'), 
+(3, 1, 50.00, '2025-03-12 20:43:00');
 
 CREATE TABLE alerts (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -45,11 +69,18 @@ CREATE TABLE alerts (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_users) REFERENCES users(id) ON DELETE CASCADE
 );
+INSERT INTO alerts (Message, is_read, id_users, created_at) 
+VALUES 
+('Votre solde est inférieur à 100€.', FALSE, 1, '2025-10-03'),
+('Un virement de 500€ a été reçu.', TRUE, 2, '2025-11-03'),
+('Attention : transaction suspecte détectée.', FALSE, 3, '2025-08-01');
 
 CREATE TABLE category (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    category ENUM('salaire','loyer','alimentation','loisirs','autres') DEFAULT NULL);
+id INT AUTO_INCREMENT PRIMARY KEY,
+type_category ENUM('salaire','loyer','alimentation','loisirs','autres') DEFAULT NULL);
 
+INSERT INTO category(type_category)
+VALUES ('Alimentation', 'Abonnements', 'Loisirs', 'Santé', 'Éducation');
 
 CREATE TABLE banker(
 id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -60,46 +91,24 @@ password_hash VARCHAR(255) NOT NULL,
 id_users INT,
 FOREIGN KEY (id_users) REFERENCES users(id) ON DELETE CASCADE);
 
-
-
-INSERT INTO users (name, surname, email, password_hash) 
-VALUES 
-('Alice', 'Dupont', 'alice.dupont@email.com', 'motdepasse1'),
-('John', 'Doe', 'john.doe@email.com', 'motdepasse2'),
-('Emma', 'Martin', 'emma.martin@email.com', 'motdepasse3');
-
-INSERT INTO accounts (account_name, balance, id_users) 
-VALUES 
-('Dupont', 1500.00, 1),
-('Doe', 800.00, 2),
-('Martin', 3000.00, 3);
-
-INSERT INTO transactions (description, amount, transaction_date, type, id_category, id_accounts) 
-VALUES 
-('Achat supermarché', 45.30, '2025-03-18 14:30:00', 'deposit', 1, 1),
-('Virement salaire', 2000.00, '2025-03-17 16:45:00', 'transfer', 2, 2),
-('Facture internet', 35.99, '2025-03-16 08:30:00', 'withdrawal', 3, 3);
-
-INSERT INTO transfers (from_account, to_account, amount, transfert_date) 
-VALUES 
-(1, 2, 100.00, '2025-02-19 19:20:00'),
-(2, 3, 250.00, '2025-01-10 12:56;00'), 
-(3, 1, 50.00, '2025-03-12 20:43:00');
-
-INSERT INTO alerts (Message, is_read, id_users, created_at) 
-VALUES 
-('Votre solde est inférieur à 100€.', FALSE, 1, '2025-10-03'),
-('Un virement de 500€ a été reçu.', TRUE, 2, '2025-11-03'),
-('Attention : transaction suspecte détectée.', FALSE, 3, '2025-08-01');
-
-INSERT INTO category(category)
-VALUES ('Alimentation', 'Abonnements', 'Loisirs', 'Santé', 'Éducation');
-
 INSERT INTO banker (name, surname, email, password_hash, id_users)
 VALUES
 ('Sophie', 'Lemoine', 'sophie.lemoine@email.com','coucou' 1),
 ('Marc', 'Durant', 'marc.durant@email.com', 'zouzou' 2),
 ('Isabelle', 'Moreau', 'isabelle.moreau@email.com', 'loulou' 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
